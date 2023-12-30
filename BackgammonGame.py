548d926e200a0e0dd06ead9f9573189358091035
kcgroulx <87883655+kcgroulx@users.noreply.github.com>
2023-12-30 00:06:53 -0800

    Added basic movement for stones

--- BackgammonGame.py
+++ BackgammonGame.py
@@ -9,38 +9,43 @@ pygame.init()
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255,0,0)
-
+YELLOW = (255,255,0)
 BOARD_COLOR = (3, 52, 133)
 
-# Set up the display
+#Display
 WIDTH, HEIGHT = 1300, 800
 
+#Bar
 BAR_WIDTH = 100
 
+#Triangle
 TRIANGLE_WIDTH = 35
 TRIANGLE_HEIGHT = 380
+QUADRENT_WIDTH = (WIDTH - BAR_WIDTH)/2
 
+#Pieces
 PIECE_SIZE = 35
 PIECE_SPACING = 5
 PIECE_COLOR_BLACK = (20, 20, 50)
 PIECE_COLOR_WHITE = (220, 220, 220)
 
+#Roll Dice Button
+ROLL_DICE_BUTTON_WIDTH = int(BAR_WIDTH)
+rollDiceButton = pygame.Rect((WIDTH-BAR_WIDTH)/2, HEIGHT-100, ROLL_DICE_BUTTON_WIDTH, 50)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Backgammon')
 
 def SpaceToPixel(space:int):
-    x,y = 0,0
-    quad_width = (WIDTH - BAR_WIDTH) / 2
-    space_size = quad_width / 7
+    space_size = QUADRENT_WIDTH / 6
     if space < 6 and space >= 0:
-        return (space_size*(space+1), 0)
+        return (space_size*(space + 0.5), 0)
     elif space < 12:
-        return (quad_width+BAR_WIDTH + space_size*(space-5), 0)
+        return (QUADRENT_WIDTH+BAR_WIDTH + space_size*(space-5.5), 0)
     elif space < 18:
-        return (quad_width+BAR_WIDTH + space_size*(18-space), HEIGHT)
+        return (QUADRENT_WIDTH+BAR_WIDTH + space_size*(17.5-space), HEIGHT)
     elif space < 24:
-        return (space_size*(24-space), HEIGHT)
+        return (space_size*(23.5-space), HEIGHT)
     else:
         raise ValueError("Invalid space value")
 
@@ -55,46 +60,92 @@ def DrawTriangles():
 def DrawPieces(board:pg.Board):
     for i in range(pg.NUM_OF_SPACES):
         space = board.boardData[i]
+        x,y = SpaceToPixel(i)
         if(space.color != pg.PLAYER_EMPTY):
-            x,y = SpaceToPixel(i)
             for j in range(space.numOfPieces):
-                pos = (x, y + j*( 2 * PIECE_SIZE + PIECE_SPACING))
+                if i < 12:
+                    pos = (x, y + j*( 2 * PIECE_SIZE + PIECE_SPACING) + PIECE_SIZE)
+                else:
+                    pos = (x, y - j*( 2 * PIECE_SIZE + PIECE_SPACING) - PIECE_SIZE)
                 color = PIECE_COLOR_WHITE if space.color == pg.PLAYER_WHITE else PIECE_COLOR_BLACK
-                pygame.draw.circle(screen, color, pos, PIECE_SIZE )
-        
+                pygame.draw.circle(screen, color, pos, PIECE_SIZE)
+
+        if i in board.legalMoves:
+            if i < 12:
+                pos = (x, y + space.numOfPieces*( 2 * PIECE_SIZE + PIECE_SPACING) + PIECE_SIZE)
+            else:
+                pos = (x, y - space.numOfPieces*( 2 * PIECE_SIZE + PIECE_SPACING) - PIECE_SIZE)
+            pygame.draw.circle(screen, YELLOW, pos, PIECE_SIZE)  
+
 
 def DrawBar():
     square_rect = pygame.Rect((WIDTH-BAR_WIDTH)/2, 0, BAR_WIDTH, HEIGHT)
     pygame.draw.rect(screen, BLACK, square_rect)
 
+def DrawHomes():
+    square_rect = pygame.Rect(0, 0, BAR_WIDTH, HEIGHT)
+    pygame.draw.rect(screen, BLACK, square_rect)
+
 def DrawBoard(board:pg.Board):
+    screen.fill(BOARD_COLOR)
     DrawTriangles()
     DrawBar()
     DrawPieces(board)
+    DrawRollDiceButton()
+    pygame.display.flip()
+
+def DrawRollDiceButton():
+    pygame.draw.rect(screen, RED, rollDiceButton)
+
+def IsButtonPressed(pos, button):
+    x, y = pos
+    return button.left <= x <= button.right and button.top <= y <= button.bottom
+
+def PixelToSpace(pos):
+    x, y = pos
+    if x < QUADRENT_WIDTH:
+        if y < HEIGHT/2:
+            return int(x/(QUADRENT_WIDTH/6))
+        elif y > HEIGHT/2:
+            return 23 - int(x/(QUADRENT_WIDTH/6))
+    elif x > QUADRENT_WIDTH + BAR_WIDTH:
+        if y < HEIGHT/2:
+            return 6 + int((x-QUADRENT_WIDTH-BAR_WIDTH)/(QUADRENT_WIDTH/6))
+        elif y > HEIGHT/2:
+            return 17 - int((x-QUADRENT_WIDTH-BAR_WIDTH)/(QUADRENT_WIDTH/6))
+    return -1
 
 def main():
     clock = pygame.time.Clock()
     running = True
 
     board = pg.Board()
-    board.boardData[0].color = pg.PLAYER_WHITE
-    board.boardData[0].numOfPieces = 4
 
-    board.boardData[2] = pg.Space(pg.PLAYER_BLACK,2)
+    board.SetBoard()
 
-    board.boardData[7].color = pg.PLAYER_BLACK
-    board.boardData[7].numOfPieces = 3
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        screen.fill(BOARD_COLOR)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if IsButtonPressed(pygame.mouse.get_pos(), rollDiceButton):
+                   board.RollDice()
+                   print(board.dice)
+                
+                spacePressed = PixelToSpace(pygame.mouse.get_pos())
+                if spacePressed != -1:
+                    if spacePressed in board.legalMoves:
+                        index = board.legalMoves.index(spacePressed)
+                        board.MakeMove(index)
+                        print("Move Made")
+                    board.SelectSpace(spacePressed)
+                    print(board.legalMoves)
+                
         DrawBoard(board)
-
-        pygame.display.flip()
+        
         clock.tick(60)
+    
     pygame.quit()
     sys.exit()
 
--- Pygammon.py
+++ Pygammon.py
@@ -1,3 +1,7 @@
+from enum import Enum
+import random
+
+
 
 NUM_OF_SPACES = 24
 
@@ -5,13 +9,94 @@ PLAYER_WHITE = 0
 PLAYER_BLACK = 1
 PLAYER_EMPTY = 255
 
+#Game states
+STATE_INIT = 0
+STATE_ROLL = 1
+STATE_MOVE = 2
+
 class Space:
     def __init__(self, color:int, numOfPieces:int):
         self.color = color
         self.numOfPieces = numOfPieces
+    
+    def RemovePiece(self):
+        self.numOfPieces -= 1
+        if self.numOfPieces < 1:
+            self.color = PLAYER_EMPTY
+    
+    def AddPiece(self, color):
+        if self.color == color or self.color == PLAYER_EMPTY:
+            self.numOfPieces += 1
+        else:
+            self.numOfPieces = 1
+        self.color = color
 
-class Board:    
+class Board:
     def __init__(self):
         self.boardData = [Space(PLAYER_EMPTY,0) for _ in range(NUM_OF_SPACES)]
-
+        self.gameState = STATE_INIT
+        self.playerTurn = PLAYER_WHITE
+        self.dice = []
+        self.legalMoves = []
+        self.selectedSpace = -1
+        self.barBlack = 0
+        self.barWhite = 0
     
+    def SetBoard(self):
+        self.boardData[0] = Space(PLAYER_WHITE, 2)
+        self.boardData[11] = Space(PLAYER_WHITE, 5)
+        self.boardData[16] = Space(PLAYER_WHITE, 3)
+        self.boardData[18] = Space(PLAYER_WHITE, 5)
+        self.boardData[23] = Space(PLAYER_BLACK, 2)
+        self.boardData[12] = Space(PLAYER_BLACK, 5)
+        self.boardData[7] = Space(PLAYER_BLACK, 3)
+        self.boardData[5] = Space(PLAYER_BLACK, 5)
+
+    def MovePiece(self, index):
+        self.boardData[index].AddPiece(self.playerTurn)
+        
+
+    def NextTurn(self):
+        self.playerTurn = PLAYER_WHITE if self.playerTurn == PLAYER_BLACK else PLAYER_BLACK
+
+    def RollDice(self):
+        die1 = random.randint(1,6)
+        die2 = random.randint(1,6)
+        if die1 == die2:
+            self.dice = [die1 for _ in range(4)]
+        else:
+            self.dice = [die1, die2]
+        self.dice.sort()
+
+    def GenerateLegalMoves(self, index:int):
+        space = self.boardData[index]
+        legalMoves = []
+        if space.color == self.playerTurn:
+            moveSet = set(self.dice)
+            for move in moveSet:
+                target = self.boardData[index + move] if self.playerTurn == PLAYER_WHITE else self.boardData[index - move]
+
+                if target.color == self.playerTurn or target.color == PLAYER_EMPTY or (target.numOfPieces == 1 and target.color != self.playerTurn):
+                    if self.playerTurn == PLAYER_WHITE:
+                        legalMoves.append(index + move)
+                    elif self.playerTurn == PLAYER_BLACK:
+                        legalMoves.append(index - move)
+        return legalMoves
+
+    def SelectSpace(self, index:int):
+        self.legalMoves = self.GenerateLegalMoves(index)
+        self.selectedSpace = index
+    
+    def MakeMove(self, index):
+        if index > len(self.legalMoves):
+            raise ValueError("Invalid legal move index")
+        
+        self.boardData[self.selectedSpace].RemovePiece()
+        self.MovePiece(self.legalMoves[index])
+
+        self.dice.pop(index)
+        if len(self.dice) == 0:
+            self.NextTurn()
+            print(self.playerTurn)
+
+        
